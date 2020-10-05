80abb25222ad56d6206ecd24098251ee9f334435
Clara Guo <guoguo.clara@gmail.com>
2020-10-05 11:38:12 -0400

    Added basic functional code to extractScores.py

---
+++ ClinicalScoreExtraction/extractScores.py
@@ -0,0 +1,31 @@
+import os
+import pandas as pd
+import numpy as np
+
+def getClinicalScores(rootdir):
+
+    scoreDataList = []
+
+    for subdir, dirs, files in os.walk(rootdir):
+        for file in files:
+            # TODO: CHANGE THE NAME BELOW  TO ClinicalAssessment
+            if file.find("ClinicalAssessment") != -1 and file.endswith(".xlsx"):
+                filename = os.path.join(subdir, file)
+                data = pd.read_excel(filename)
+                df = pd.DataFrame(data)
+                scoreDataList.append(df)
+                # print(filename)
+
+
+    result = pd.concat(scoreDataList)
+    scoreSum = np.cumsum(result,axis=0)[1:]
+    # print(np.cumsum(result,axis=0)[-1:])
+    scoreMean = result.mean()
+    print(scoreMean)
+
+    # print(result)
+
+
+if __name__ == '__main__':
+    directoryPath = '/Users/Clara/Desktop/KiMoRe-Full'
+    getClinicalScores(directoryPath)
